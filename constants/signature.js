7e0f952dd41c4173222d7ef284a1847a786e225a
aniketh <aniket@helpshift.com>
2018-07-26 09:54:47 +0530

    Avoid creating const for signature symbols

--- constants/signature.js
+++ constants/signature.js
@@ -1,15 +1,8 @@
-const WILDCARD = Symbol ();
-const TRUTHY = Symbol ();
-const FALSY = Symbol ();
-const INSTANCE_OF = Symbol ();
-const SHAPE = Symbol ();
-const ARRAY_OF = Symbol ();
-
 exports.symbols = {
-  WILDCARD,
-  TRUTHY,
-  FALSY,
-  INSTANCE_OF,
-  SHAPE,
-  ARRAY_OF
+  WILDCARD: Symbol (),
+  TRUTHY: Symbol (),
+  FALSY: Symbol (),
+  INSTANCE_OF: Symbol (),
+  SHAPE: Symbol (),
+  ARRAY_OF: Symbol ()
 };
